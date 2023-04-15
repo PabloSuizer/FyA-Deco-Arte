3dc24ea87944f6f18b4a14e2d51d272a48bebea7
Juan Pablo Suizer <suizerpablo@gmail.com>
2023-04-15 20:37:50 -0300

    Fixed Creacion de Pages y Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import './App.css'
-import Home from './Pages/Home/Home'
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Accesorios from './Pages/Accesorios/Accesorios';
+import Mates from './Pages/Mates/Mates';
+import Mayorista from './Pages/Mayorista/Mayorista';
+import Productos  from './Pages/Productos/Productos';
+import SignIn from './Pages/SignIn/SignIn';
+import SignUp from './Pages/SignUp/SignUp';
+
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <ResponsiveAppBar />
+      <Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Accesorios' element={<Accesorios/>}/>
+        <Route path='/Mates' element={<Mates/>}/>
+        <Route path='/Mayorista' element={<Mayorista/>}/>
+        <Route path='/Productos' element={<Productos/>}/>
+        <Route path='/SignIn' element={<SignIn/>}/>
+        <Route path='/SignUp' element={<SignUp/>}/>
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Route>
+      <StickyFooter/>
+    </BrowserRouter>
+
   )
 }
 
